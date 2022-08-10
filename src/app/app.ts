0a887605831ca2821b0646069c0d32726b48a2c6
Cydonian <cydonian_07@hotmail.com>
2022-08-10 10:55:28 -0500

    [myArray] probando resultados

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,3 +1,16 @@
+import { MyArray } from './estructura-datos/Array'
+
 export function main() {
+  const myArray = new MyArray<string | number>()
+  const resp1 = myArray.push( "gabriel" )
+  myArray.push( "mabe" )
+  myArray.push( "rafael" )
+  myArray.push( "fiorella" )
+  console.log( resp1 );
+  const resp2 = myArray.get( 1 )
+  console.log( resp2 );
+  myArray.delete( 2 )
+
+  console.log( myArray );
 
 }
---
+++ src/app/estructura-datos/Array.ts
@@ -0,0 +1,75 @@
+
+
+export class MyArray<T> {
+
+  public length: number
+  private data: Record<number, T>
+
+  constructor() {
+    this.length = 0
+    this.data = {}
+  }
+
+  push( item: T ) {
+    this.data[ this.length ] = item
+    this.length++
+    return this.data
+  }
+
+  get( index: number ) {
+    if ( !this.data[ index ] ) {
+      return null
+    }
+    return this.data[ index ]
+  }
+
+  /**
+   * Elimina el ultimo elemento
+   */
+  pop() {
+    const lastItem = this.data[ this.length - 1 ]
+    delete this.data[ this.length - 1 ]
+    this.length--
+    return lastItem
+  }
+
+
+  /**
+   * Elimina un item segun su indice
+   * @param index
+   */
+  delete( index: number ) {
+    const item = this.data[ index ]
+    this.shiftIndex( index )
+    return item
+  }
+
+  /**
+   * Elimina el primer elemento del arreglo
+   */
+  shift() {
+    const firstItem = this.data[ 0 ]
+    this.shiftIndex( 0 )
+    return firstItem
+  }
+
+  /**
+   * Agrega elementos al inicio del arreglo
+   */
+  unshif( item: T ) {
+    for ( let i = 0; i < this.length; i++ ) {
+      this.data[ i ] = this.data[ i + 1 ]
+    }
+    this.data[ 0 ] = item
+    return this.data
+  }
+
+  shiftIndex( index: number ) {
+    for ( let i = index; i < this.length; i++ ) {
+      this.data[ i ] = this.data[ i + 1 ]
+    }
+    delete this.data[ this.length - 1 ]
+    this.length--
+  }
+}
+
